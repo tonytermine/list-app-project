068f3a9fffe5d6c0c346eccb748673f2a76ae6a3
Tony Termine <termine.tony@gmail.com>
2014-04-07 12:58:18 -0400

    Finished Non-functional design
    Starting to add in the necessary jQuery to make this app functional.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+	// enter a new list item by entering it into the input field and clicking on the + button.
+	$(".add").click(function() {
+		var toAdd = $("input[class=main-input]").val();
+		$("ul").append("<li class='items'>"+toAdd+"</li>");
+		event.preventDefault();
+	});
+});
